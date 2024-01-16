4ba749c2bc196bb066fc017df96106f4092a76b2
ossegosse <ossian.igelberg@gmail.com>
2024-01-16 13:57:56 +0100

    added object

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 var animals = ["apa", "orangutang", "gorilla", "get", "giraff"];
 console.log(animals);
+var person = {
+    name: "Ossian Igelberg",
+    age: 31,
+    student: true
+};
+console.log(person);
+//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,2 +1,19 @@
 let animals: string[] = ["apa", "orangutang", "gorilla", "get", "giraff"]
 console.log(animals)
+
+// Skapa ett objekt med tre egenskaper. En sträng, en siffra och en boolean. 
+// Skriv ut objektet till console och testkör.
+
+type Person = {
+    name: string;
+    age: number;
+    student: boolean;
+}
+
+let person: Person = {
+    name: "Ossian Igelberg",
+    age: 31,
+    student: true
+
+}
+console.log(person)
