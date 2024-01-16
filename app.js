008432880846640689088b903868366b89a1d377
ossegosse <ossian.igelberg@gmail.com>
2024-01-16 14:29:58 +0100

    added ts

--- app.js
+++ app.js
@@ -6,4 +6,18 @@ var person = {
     student: true
 };
 console.log(person);
+//Skriv en funktion som tar emot en sträng och returnerar en sträng. 
+//Om funktionen tar emot strängen “X” ska den returnera strängen “hej X”. 
+// Funktionen ska kunna ta emot vilken sträng som helst och skicka tillbaka en sträng med hej först.
+function returnString(x) {
+    if (x)
+        return ("hej" + x);
+}
+console.log(returnString(" ossian igelberg, 31 år"));
+// Loopa igenom arrayen av strängar och kör funktionen på varje sträng 
+// och skriv ut till konsollen det du får från arrayen.
+for (var i = 0; i < animals.length; i++) {
+    var result = returnString(animals[i]);
+    console.log(result);
+}
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -17,3 +17,20 @@ let person: Person = {
 
 }
 console.log(person)
+
+//Skriv en funktion som tar emot en sträng och returnerar en sträng. 
+//Om funktionen tar emot strängen “X” ska den returnera strängen “hej X”. 
+// Funktionen ska kunna ta emot vilken sträng som helst och skicka tillbaka en sträng med hej först.
+function returnString (x: string) {
+    
+    if (x) return ("hej" + x)
+}
+
+console.log(returnString(" ossian igelberg, 31 år"))
+
+// Loopa igenom arrayen av strängar och kör funktionen på varje sträng 
+// och skriv ut till konsollen det du får från arrayen.
+for (let i = 0; i < animals.length; i++) {
+    const result = returnString(animals[i])
+    console.log(result)
+}
